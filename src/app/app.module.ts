9adf9080f1ad904e1d09804328e4547031105cbb
lost-one-fish <gaduo.chien@gmail.com>
2018-03-11 17:45:27 +0800

    [Highcharts] Highcharts & ng2-Highcharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatListModule, MatSidenavModule } from '@angular/material';
+import { ChartModule } from 'angular2-highcharts';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { MatButtonModule, MatCheckboxModule, MatListModule, MatSidenavModule } f
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule
+    MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule,
+    ChartModule.forRoot(require('highcharts/highstock'))
   ],
   providers: [],
   bootstrap: [AppComponent]
