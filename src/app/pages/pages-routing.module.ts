439251e890031b78e3c0c492de0382eca9500737
Gaduo <gaduo.chien@gmail.com>
2018-06-04 07:04:35 +0800

    add Routing of System Settings

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -14,6 +14,9 @@ const routes: Routes = [{
     path: 'ui-features',
     loadChildren: './ui-features/ui-features.module#UiFeaturesModule',
   }, {
+    path: 'system-settings',
+    loadChildren: './system-settings/system-settings.module#SystemSettingsModule',
+  }, {
     path: 'components',
     loadChildren: './components/components.module#ComponentsModule',
   }, {
