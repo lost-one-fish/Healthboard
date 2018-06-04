0414f9ac9f2dbf67fc6c52e3522dc7b9fefed03f
Gaduo <gaduo.chien@gmail.com>
2018-06-04 11:06:15 +0800

    [修改] 刪除 Social Links

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -9,21 +9,6 @@ import { DataModule } from './data/data.module';
 import { AnalyticsService } from './utils/analytics.service';
 
 const socialLinks = [
-  {
-    url: 'https://github.com/akveo/nebular',
-    target: '_blank',
-    icon: 'socicon-github',
-  },
-  {
-    url: 'https://www.facebook.com/akveo/',
-    target: '_blank',
-    icon: 'socicon-facebook',
-  },
-  {
-    url: 'https://twitter.com/akveo_inc',
-    target: '_blank',
-    icon: 'socicon-twitter',
-  },
 ];
 
 const NB_CORE_PROVIDERS = [
