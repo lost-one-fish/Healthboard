6253ca73a1bddec16ace918be8a056666aa8091e
Gaduo <gaduo.chien@gmail.com>
2018-06-06 09:05:21 +0800

    [修改] Getting User Info

--- src/app/@theme/components/header/header.component.ts
+++ src/app/@theme/components/header/header.component.ts
@@ -3,6 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NbMenuService, NbSidebarService } from '@nebular/theme';
 import { UserService } from '../../../@core/data/users.service';
 import { AnalyticsService } from '../../../@core/utils/analytics.service';
+import { NbAuthJWTToken, NbAuthService } from '@nebular/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ngx-header',
@@ -21,12 +23,21 @@ export class HeaderComponent implements OnInit {
   constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
               private userService: UserService,
+              private authService: NbAuthService,
+              private router: Router,
               private analyticsService: AnalyticsService) {
   }
 
   ngOnInit() {
-    this.userService.getUsers()
-      .subscribe((users: any) => this.user = users.nick);
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+        if (token.isValid()) {
+          // here we receive a payload from the token and assigne it to our `user` variable
+          this.user = token.getPayload();
+        } else {
+          this.router.navigate(['auth/login']);
+        }
+      });
   }
 
   toggleSidebar(): boolean {
