98f3be81becdcc4f80739c72fd730f278722513c
Gaduo <gaduo.chien@gmail.com>
2018-06-06 09:05:02 +0800

    [修改] the settings of (login, register, logout, reset-pass, refresh-token)

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -1,12 +1,13 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbAuthModule, NbDummyAuthProvider } from '@nebular/auth';
+import { NbAuthModule, NbEmailPassAuthProvider } from '@nebular/auth';
 import { NbSecurityModule, NbRoleProvider } from '@nebular/security';
 import { of as observableOf } from 'rxjs/observable/of';
 
 import { throwIfAlreadyLoaded } from './module-import-guard';
 import { DataModule } from './data/data.module';
 import { AnalyticsService } from './utils/analytics.service';
+import { Configuration } from '../app-config';
 
 const socialLinks = [
 ];
@@ -16,21 +17,98 @@ const NB_CORE_PROVIDERS = [
   ...NbAuthModule.forRoot({
     providers: {
       email: {
-        service: NbDummyAuthProvider,
+        service: NbEmailPassAuthProvider,
         config: {
           delay: 3000,
+          baseEndpoint: Configuration.authServer,
           login: {
+            alwaysFail: false,
             rememberMe: true,
+            'endpoint': '/api/auth/token',
+            method: 'post',
+            redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['Login/Email combination is not correct, please try again.'],
+            defaultMessages: ['You have been successfully logged in.'],
+          },
+          register: {
+            alwaysFail: false,
+            rememberMe: true,
+            endpoint: '/api/auth/register',
+            method: 'post',
+            redirect: {
+              success: '/',
+              failure: null,
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['You have been successfully registered.'],
+          },
+          logout: {
+            alwaysFail: false,
+            endpoint: '/api/auth/logout',
+            method: 'post',
+            redirect: {
+              success: '/auth/login',
+              failure: '/auth/login',
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['You have been successfully logged out.'],
+          },
+          resetPass: {
+            endpoint: '/api/auth/reset-pass',
+            method: 'put',
+            redirect: {
+              success: '/',
+              failure: null,
+            },
+            resetPasswordTokenKey: 'reset_password_token',
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['Your password has been successfully changed.'],
+          },
+          refreshToken: {
+            endpoint: '/api/auth/refresh-token',
+            method: 'post',
+            redirect: {
+              success: null,
+              failure: null,
+            },
+            defaultErrors: ['Something went wrong, please try again.'],
+            defaultMessages: ['Your token has been successfully refreshed.'],
+          },
+          token: {
+            key: 'access_token', // this parameter tells Nebular where to look for the token
+          },
+          errors: {
+            key: 'errors',
+          },
+          messages: {
+            key: 'messages',
           },
         },
       },
     },
     forms: {
       login: {
-        socialLinks: socialLinks,
+        // delay before redirect after a successful login, while success message is shown to the user
+        redirectDelay: 500,
+        provider: 'email',  // provider id key. If you have multiple providers, or what to use your own
+        rememberMe: true,   // whether to show or not the `rememberMe` checkbox
+        showMessages: {     // show/not show success/error messages
+          success: true,
+          error: true,
+        },
+        socialLinks: socialLinks, // social links at the bottom of a page
       },
       register: {
         socialLinks: socialLinks,
+      },
+      validation: {
+        password: {
+          required: true,
+          minLength: 3,
+        },
       },
     },
   }).providers,
