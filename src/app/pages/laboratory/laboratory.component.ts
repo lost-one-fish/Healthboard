0a562d0f4cf9e75bfaf794447157a02829fcdae9
PickOneFish <pickonefish@gmail.com>
2018-08-11 11:41:58 +0800

    fix: remove scss urls

--- src/app/pages/laboratory/laboratory.component.ts
+++ src/app/pages/laboratory/laboratory.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngx-laboratory',
   template: '<router-outlet></router-outlet>',
-  styleUrls: ['./laboratory.component.scss'],
 })
 export class LaboratoryComponent implements OnInit {
 
