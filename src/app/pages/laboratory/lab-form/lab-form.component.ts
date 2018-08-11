06fa88049fdb4d8708036c9b97be187c6772a087
PickOneFish <pickonefish@gmail.com>
2018-08-11 18:50:10 +0800

    feat: change form template

--- src/app/pages/laboratory/lab-form/lab-form.component.ts
+++ src/app/pages/laboratory/lab-form/lab-form.component.ts
@@ -10,9 +10,13 @@ export class LabFormComponent implements OnInit {
   @Input()
   title;
 
+  format = '#0.## 公分';
+  now;
+
   constructor() { }
 
   ngOnInit() {
+    this.now = new Date();
   }
 
 }
--- src/app/pages/laboratory/laboratory.module.ts
+++ src/app/pages/laboratory/laboratory.module.ts
@@ -7,6 +7,7 @@ import { LaboratoryComponent } from './laboratory.component';
 import { LabFormComponent } from './lab-form/lab-form.component';
 import { LabListComponent } from './lab-list/lab-list.component';
 import { ThemeModule } from '../../@theme/theme.module';
+import { DxDateBoxModule, DxNumberBoxModule, DxTextBoxModule } from 'devextreme-angular';
 
 const components = [
   LaboratoryComponent,
@@ -15,11 +16,13 @@ const components = [
   LabListComponent,
 ];
 
-
 @NgModule({
   imports: [
     CommonModule,
     ThemeModule,
+    DxTextBoxModule,
+    DxNumberBoxModule,
+    DxDateBoxModule,
     LaboratoryRoutingModule,
   ],
   declarations: [...components],
