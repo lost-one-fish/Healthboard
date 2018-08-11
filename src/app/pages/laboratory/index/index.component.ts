c70ab8576fcfc12dc351e40d78b9aa6cf827ab06
PickOneFish <pickonefish@gmail.com>
2018-08-11 17:39:59 +0800

    feat: add classifications in the tab-set

--- src/app/pages/laboratory/index/index.component.ts
+++ src/app/pages/laboratory/index/index.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  classifications = ['身高', '體重', 'BMI', '體溫', '脈搏/心跳',
+    '血氧', '飯前血糖', '收縮壓', '舒張壓', '低密度蛋白膽固醇',
+    '總膽固醇', '腎絲球過濾率', '糖化血紅素過濾率', '肌酸酐'];
+
+  selectedClassification = this.classifications[0];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onSelectClassification(selected) {
+    this.selectedClassification = selected;
+  }
 }
