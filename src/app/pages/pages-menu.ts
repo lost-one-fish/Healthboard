de0eec5d4e9a74fabe2749cae96874680c5e680a
PickOneFish <pickonefish@gmail.com>
2018-08-13 09:15:28 +0800

    chore: 暫時隱藏 menu item

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -48,30 +48,31 @@ export const MENU_ITEMS: NbMenuItem[] = [
       resource: 'history',
     },
   },
-  {
-    title: '系統設定',
-    icon: 'fa fa fa-wrench',
-    link: '/pages/system-settings',
-    data: {
-      permission: 'view',
-      resource: 'system-settings',
-    },
-    children: [
-      {
-        icon: 'fa fa-user',
-        title: '帳號管理',
-        link: '/pages/system-settings/manage-user',
-      },
-      {
-        icon: 'fa fa-key',
-        title: '密碼變更',
-        link: '/pages/system-settings/change-password',
-      },
-      {
-        icon: 'fa fa-sitemap',
-        title: '單位管理',
-        link: '/pages/system-settings/manage-organization',
-      },
-    ],
-  },
+  // FIXME: 暫時隱藏 menu item
+  // {
+  //   title: '系統設定',
+  //   icon: 'fa fa fa-wrench',
+  //   link: '/pages/system-settings',
+  //   data: {
+  //     permission: 'view',
+  //     resource: 'system-settings',
+  //   },
+  //   children: [
+  //     {
+  //       icon: 'fa fa-user',
+  //       title: '帳號管理',
+  //       link: '/pages/system-settings/manage-user',
+  //     },
+  //     {
+  //       icon: 'fa fa-key',
+  //       title: '密碼變更',
+  //       link: '/pages/system-settings/change-password',
+  //     },
+  //     {
+  //       icon: 'fa fa-sitemap',
+  //       title: '單位管理',
+  //       link: '/pages/system-settings/manage-organization',
+  //     },
+  //   ],
+  // },
 ];
