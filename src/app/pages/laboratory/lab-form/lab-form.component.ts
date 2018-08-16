504be70203d30a184d9896af903c5656b4e57380
PickOneFish <pickonefish@gmail.com>
2018-08-16 20:25:29 +0800

    fix: create resource by subject, coding, value

--- src/app/pages/laboratory/lab-form/lab-form.component.ts
+++ src/app/pages/laboratory/lab-form/lab-form.component.ts
@@ -43,54 +43,64 @@ export class LabFormComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
 
-    if (!changes['classification']) {
+    if (changes['classification']) {
+      const value = changes['classification'].currentValue;
+      this.format = '#0.## ' + value['unit'];
+
+      this.value = 0;
+      this.now = new Date();
+
+      this.items = [{
+        location: 'before',
+        locateInMenu: 'never',
+        template: () => {
+          return '<div class=\'toolbar-label\'><h2> ' + value['class'] + '</h2></div>';
+        },
+      }, {
+        location: 'after',
+        widget: 'dxButton',
+        locateInMenu: 'auto',
+        options: {
+          icon: 'save',
+          onClick: () => {
+            if (!this.classification['code']) {
+              notify('Not Found the code');
+              return;
+            }
+            if (!this.classification['unit']) {
+              notify('Not Found the unit');
+              return;
+            }
+            if (this.value <= 0) {
+              notify('The value is empty.');
+              return;
+            }
+
+            const resource = Object.assign(this.data, {
+              'effectiveDateTime': this.now,
+              'valueQuantity': {
+                'value': this.value,
+                'unit': this.classification['unit'],
+              },
+            });
+
+            this.resource.emit(resource);
+          },
+        },
+      }];
+
+      this.data = Object.assign(this.data, {
+        'code': {
+          'coding': [
+            {
+              'code': value['code'],
+              'display': value['class'],
+            },
+          ],
+        },
+      });
       return;
     }
-
-    const value = changes['classification'].currentValue;
-    this.format = '#0.## ' + value['unit'];
-
-    this.value = 0;
-    this.now = new Date();
-
-    this.items = [{
-      location: 'before',
-      locateInMenu: 'never',
-      template: () => {
-        return '<div class=\'toolbar-label\'><h2> ' + value['class'] + '</h2></div>';
-      },
-    }, {
-      location: 'after',
-      widget: 'dxButton',
-      locateInMenu: 'auto',
-      options: {
-        icon: 'save',
-        onClick: () => {
-          if (!this.classification['code']) {
-            notify('Not Found the code');
-            return;
-          }
-          if (!this.classification['unit']) {
-            notify('Not Found the unit');
-            return;
-          }
-          if (this.value <= 0) {
-            notify('The value is empty.');
-            return;
-          }
-
-          const resource = Object.assign(this.data, {
-            'effectiveDateTime': this.now,
-            'valueQuantity': {
-              'value': this.value,
-              'unit': this.classification['unit'],
-            },
-          });
-
-          this.resource.emit(resource);
-        },
-      },
-    }];
 
     if (changes['subject'] && changes['subject'].currentValue) {
       this.data = Object.assign(this.data, {
@@ -98,18 +108,8 @@ export class LabFormComponent implements OnInit, OnChanges {
           reference: 'Patient/' + changes['subject'].currentValue.id,
         },
       });
+      return;
     }
-
-    this.data = Object.assign(this.data, {
-      'code': {
-        'coding': [
-          {
-            'code': value['code'],
-            'display': value['class'],
-          },
-        ],
-      },
-    });
   }
 
   onUpdate(event) {
