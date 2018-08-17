c65b49bf6bc5601e1a1a2820f6ad8701579f8d76
PickOneFish <pickonefish@gmail.com>
2018-08-17 23:28:56 +0800

    fix: CUR resource to server

--- src/app/pages/case-management/index/index.component.ts
+++ src/app/pages/case-management/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PatientRestService } from '../../../@fhir/patient-rest.service';
+import notify from '../../../../../node_modules/devextreme/ui/notify';
 
 @Component({
   selector: 'ngx-index',
@@ -16,6 +17,53 @@ export class IndexComponent implements OnInit {
 
   ngOnInit() {
     this.fetchData();
+  }
+
+  onCreatePatient(resource) {
+    this.patientService.create({
+      resource: resource,
+    }).subscribe(next => {
+      notify('新增成功');
+
+      let created: boolean = true;
+      this.dataSet = this.dataSet.map(item => {
+        if (item.id === resource.id) {
+          created = false;
+          return Object.assign({}, item, resource);
+        } else {
+          return item;
+        }
+      });
+      if (created) {
+        this.dataSet.reverse();
+        this.dataSet.push(next.data);
+        this.dataSet.reverse();
+      }
+    });
+  }
+
+  onUpdatePatient(resource) {
+    this.patientService.update({
+      resource: resource,
+    }).subscribe(next => {
+      notify('更新成功');
+
+      this.dataSet = this.dataSet.map(item => {
+        if (item.id === resource.id) {
+          return Object.assign({}, item, resource);
+        } else {
+          return item;
+        }
+      });
+    });
+  }
+
+  onDeletePatient(resource) {
+    this.patientService.delete({
+      resource: resource,
+    }).subscribe(next => {
+      notify('刪除成功');
+    });
   }
 
   fetchData() {
