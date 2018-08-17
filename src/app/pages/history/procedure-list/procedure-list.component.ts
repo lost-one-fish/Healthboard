edc5e2d642b8782715bf040c5dcec579a4257ad6
PickOneFish <pickonefish@gmail.com>
2018-08-17 16:23:40 +0800

    fix: create form for resource creating or resource updating

--- src/app/pages/history/procedure-list/procedure-list.component.ts
+++ src/app/pages/history/procedure-list/procedure-list.component.ts
@@ -30,6 +30,37 @@ export class ProcedureListComponent implements OnInit, OnChanges {
 
   dataSource: DataSource;
 
+  statusItems = [
+    {
+      key: 'preparation',
+      value: '預備',
+    },
+    {
+      key: 'in-progress',
+      value: '進行中',
+    },
+    {
+      key: 'suspended',
+      value: '暫停',
+    },
+    {
+      key: 'aborted',
+      value: '中止',
+    },
+    {
+      key: 'completed',
+      value: '完成',
+    },
+    {
+      key: 'entered-in-error',
+      value: '輸入錯誤',
+    },
+    {
+      key: 'unknown',
+      value: '未知',
+    },
+  ];
+
   constructor() {
   }
 
@@ -40,6 +71,17 @@ export class ProcedureListComponent implements OnInit, OnChanges {
     if (changes['dataSet']) {
       this.dataSource = new DataSource(changes['dataSet'].currentValue);
     }
+  }
+
+  setCodingCode(rawData, val) {
+    rawData.code = {'coding': [{}]};
+    rawData.code.coding[0] = {
+      display: val,
+    };
+  }
+
+  setStatusValue(rawData, val) {
+    rawData.status = val;
   }
 
   onRowInserting(e) {
