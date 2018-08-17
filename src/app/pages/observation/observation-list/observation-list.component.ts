1439ec670139d573df602e48a3783e1ceef09a49
PickOneFish <pickonefish@gmail.com>
2018-08-17 18:12:16 +0800

    fix: change to code.text

--- src/app/pages/observation/observation-list/observation-list.component.ts
+++ src/app/pages/observation/observation-list/observation-list.component.ts
@@ -16,6 +16,23 @@ import DataSource from '../../../../../node_modules/devextreme/data/data_source'
 })
 export class ObservationListComponent implements OnInit, OnChanges {
 
+  classifications = {
+    '3137-7': '身高',
+    '3141-9': '體重',
+    '39156-5': 'BMI',
+    '8310-5': '體溫',
+    '8867-4': '脈搏/心跳',
+    '1556-0': '血氧',
+    '62851-1': '飯前血糖',
+    '8480-6': '收縮壓',
+    '8462-4': '舒張壓',
+    '2089-1': '低密度蛋白膽固醇',
+    '2093-3': '總膽固醇',
+    '33914-3': '腎絲球過濾率',
+    '4548-4': '糖化血紅素過濾率',
+    '2160-0': '肌酸酐',
+  };
+
   @Input()
   dataSet = [];
 
@@ -31,7 +48,11 @@ export class ObservationListComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['dataSet']) {
-      this.dataSource = new DataSource(changes['dataSet'].currentValue);
+      const data = changes['dataSet'].currentValue.map(resource => {
+        resource.code.text = this.classifications[resource.code.coding[0].code];
+        return resource;
+      });
+      this.dataSource = new DataSource(data);
     }
   }
 
